be61e9f05322e9024ab80e7ad87558bb7e4838d1
KANGKYEONGSEOK <rudtjr1303@naver.com>
2022-10-28 18:24:28 +0900

    [refactor] 헤더컨트롤러 주석
    헤더컨트롤러 주석

--- rence/src/main/java/test/com/rence/user/HeaderController.java
+++ rence/src/main/java/test/com/rence/user/HeaderController.java
@@ -4,8 +4,11 @@
  * 회원 탈퇴
  */
 
-
 package test.com.rence.user;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,23 +22,34 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HeaderController {
 
 	private static final Logger logger = LoggerFactory.getLogger(HeaderController.class);
-	
+
 	@Autowired
 	UserSerivice service;
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	//마이페이지 이동
 	@RequestMapping(value = "/go_my_page", method = RequestMethod.GET)
-	public String go_my_page(Model model, UserVO uvo) {
+	public String go_my_page(Model model, HttpServletRequest request) {
 		logger.info("go_my_page()...");
 		
-		UserMypageVO umvo = service.user_mypage_select(uvo);
-		
-		model.addAttribute("umvo",umvo);
+//		String user_no = null;
+//		UserVO uvo = new UserVO();
+//		
+//		Cookie[] cookies = request.getCookies();
+//		for(Cookie c : cookies) {
+//			if(c.getName().equals("user_no")) {
+//				user_no = c.getValue();
+//			}
+//		  }
+//		uvo.setUser_no(user_no);
+//		
+//		UserMypageVO umvo = service.user_mypage_select(uvo);
+//		
+//		model.addAttribute("umvo",umvo);
 
 		return ".my_page/my_page";
 	}
 
-}//end class
+}// end class
