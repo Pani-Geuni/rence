ae362946f5fcda5a2e8eb9cb0b1ce609f23f6ba9
PanGeun <lifebook0809@gmail.com>
2022-10-29 17:50:20 +0900

    [refacctor] Dash Board Package 생성
    - test.com.rence.dashboard 생성 및 DashBoardController 이동

--- rence/src/main/java/test/com/rence/dashboard/DashBoardController.java
+++ rence/src/main/java/test/com/rence/dashboard/DashBoardController.java
@@ -1,4 +1,4 @@
-package test.com.rence.backoffice;
+package test.com.rence.dashboard;
 
 import javax.servlet.ServletContext;
 
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import test.com.rence.backoffice.BackOfficeController;
 
 @Controller
 public class DashBoardController {
