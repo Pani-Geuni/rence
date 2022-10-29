a2f3d5c56736926fc195880eaca948a27397df73
PanGeun <lifebook0809@gmail.com>
2022-10-29 17:37:51 +0900

    [feat] BackOffice tiles 생성

---
+++ rence/src/main/java/test/com/rence/backoffice/DashBoardController.java
@@ -0,0 +1,25 @@
+package test.com.rence.backoffice;
+
+import javax.servlet.ServletContext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class DashBoardController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(BackOfficeController.class);
+	
+	@Autowired
+	ServletContext context;
+	
+	
+	@RequestMapping(value = "/backoffice_home", method = RequestMethod.GET)
+	public String dashboard_main() {
+		return ".dash_board/home";
+	}
+}
