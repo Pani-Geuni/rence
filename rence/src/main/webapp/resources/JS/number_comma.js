1ab4dc115a03613f55d2cb47a1b1eb6295a758f7
PanGeun <lifebook0809@gmail.com>
2022-10-29 17:38:37 +0900

    [UI] Dash Board UI 작업
    - css UI 작업 추가
    - side menu 및 숫자 정규화 js 파일 추가

---
+++ rence/src/main/webapp/resources/JS/number_comma.js
@@ -0,0 +1,55 @@
+$(function () {
+  $('.sales-income').text(
+    $('.sales-income')
+      .text()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  );
+
+  $('.sales-revenue').text(
+    $('.sales-revenue')
+      .text()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  );
+
+  $('.sales-cancel').text(
+    $('.sales-cancel')
+      .text()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  );
+
+  $('.sales-price').each(function (index, value) {
+    let price = $(value).text();
+
+    $(this).text(price.replace(/\B(?=(\d{3})+(?!\d))/g, ','));
+  });
+
+  $('.reserve-price').each(function (index, value) {
+    let price = $(value).text();
+
+    $(this).text(price.replace(/\B(?=(\d{3})+(?!\d))/g, ','));
+  });
+
+  $('.reserve-count').text(
+    $('.reserve-count')
+      .text()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  );
+
+  $('.qna-count').text(
+    $('.qna-count')
+      .text()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  );
+
+  $('.review-count').text(
+    $('.review-count')
+      .text()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  );
+
+  $('#room-price').text(
+    $('#room-price')
+      .text()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+  );
+})
