141c2831078b039f743ab65615e2f7a5b7ce88f7
Choi <choetruth44@gmail.com>
2022-10-31 16:11:29 +0900

    [feat] BackOffice 운영시간 null 수정
    BackOffice 운영시간 null 구현 완료

--- rence/src/main/java/test/com/rence/backoffice/OperatingTime.java
+++ rence/src/main/java/test/com/rence/backoffice/OperatingTime.java
@@ -26,34 +26,62 @@ public class OperatingTime {
 		
 		CustomDateFormatter cdf = new CustomDateFormatter();
 		
-		Date mon_stime = cdf.hourFormmatter(ovo.getMon_stime());
-		ovo2.setMon_stime(mon_stime);
-		Date mon_etime = cdf.hourFormmatter(ovo.getMon_etime());
-		ovo2.setMon_etime(mon_etime);
-		Date tue_stime = cdf.hourFormmatter(ovo.getTue_stime());
-		ovo2.setTue_stime(tue_stime);
-		Date tue_etime = cdf.hourFormmatter(ovo.getTue_etime());
-		ovo2.setTue_etime(tue_etime);
-		Date wed_stime = cdf.hourFormmatter(ovo.getWed_stime());
-		ovo2.setWed_stime(wed_stime);
-		Date wed_etime = cdf.hourFormmatter(ovo.getWed_etime());
-		ovo2.setWed_etime(wed_etime);
-		Date thu_stime = cdf.hourFormmatter(ovo.getThu_stime());
-		ovo2.setThu_stime(thu_stime);
-		Date thu_etime = cdf.hourFormmatter(ovo.getThu_etime());
-		ovo2.setThu_etime(thu_etime);
-		Date fri_stime = cdf.hourFormmatter(ovo.getFri_stime());
-		ovo2.setFri_stime(fri_stime);
-		Date fri_etime = cdf.hourFormmatter(ovo.getFri_etime());
-		ovo2.setFri_etime(fri_etime);
-		Date sat_stime = cdf.hourFormmatter(ovo.getSat_stime());
-		ovo2.setSat_stime(sat_stime);
-		Date sat_etime = cdf.hourFormmatter(ovo.getSat_etime());
-		ovo2.setSat_etime(sat_etime);
-		Date sun_stime = cdf.hourFormmatter(ovo.getSun_stime());
-		ovo2.setSun_stime(sun_stime);
-		Date sun_etime = cdf.hourFormmatter(ovo.getSun_etime());
-		ovo2.setSun_etime(sun_etime);
+		if (ovo.getMon_stime()!=null) {
+			Date mon_stime = cdf.hourFormmatter(ovo.getMon_stime());
+			ovo2.setMon_stime(mon_stime);
+		}
+		if(ovo.getMon_etime()!=null) {
+			Date mon_etime = cdf.hourFormmatter(ovo.getMon_etime());
+			ovo2.setMon_etime(mon_etime);
+		}
+		if (ovo.getTue_stime()!=null) {
+			Date tue_stime = cdf.hourFormmatter(ovo.getTue_stime());
+			ovo2.setTue_stime(tue_stime);
+		}
+		if (ovo.getTue_etime()!=null) {
+			Date tue_etime = cdf.hourFormmatter(ovo.getTue_etime());
+			ovo2.setTue_etime(tue_etime);
+		}
+		if (ovo.getWed_stime()!=null) {
+			Date wed_stime = cdf.hourFormmatter(ovo.getWed_stime());
+			ovo2.setWed_stime(wed_stime);
+		}
+		if (ovo.getWed_etime()!=null) {
+			Date wed_etime = cdf.hourFormmatter(ovo.getWed_etime());
+			ovo2.setWed_etime(wed_etime);
+		}
+		if (ovo.getThu_stime()!=null) {
+			Date thu_stime = cdf.hourFormmatter(ovo.getThu_stime());
+			ovo2.setThu_stime(thu_stime);
+		}
+		if (ovo.getThu_etime()!=null) {
+			Date thu_etime = cdf.hourFormmatter(ovo.getThu_etime());
+			ovo2.setThu_etime(thu_etime);
+		}
+		if (ovo.getFri_stime()!=null) {
+			Date fri_stime = cdf.hourFormmatter(ovo.getFri_stime());
+			ovo2.setFri_stime(fri_stime);
+		}
+		if (ovo.getFri_etime()!=null) {
+			Date fri_etime = cdf.hourFormmatter(ovo.getFri_etime());
+			ovo2.setFri_etime(fri_etime);
+		}
+		if (ovo.getSat_stime()!=null) {
+			Date sat_stime = cdf.hourFormmatter(ovo.getSat_stime());
+			ovo2.setSat_stime(sat_stime);
+		}
+		if (ovo.getSat_etime()!=null) {
+			Date sat_etime = cdf.hourFormmatter(ovo.getSat_etime());
+			ovo2.setSat_etime(sat_etime);
+		}
+		if (ovo.getSun_stime()!=null) {
+			Date sun_stime = cdf.hourFormmatter(ovo.getSun_stime());
+			ovo2.setSun_stime(sun_stime);
+		}
+		if(ovo.getSun_etime()!=null) {
+			Date sun_etime = cdf.hourFormmatter(ovo.getSun_etime());
+			ovo2.setSun_etime(sun_etime);
+		}
 		
 		ovo2.setMon_dayoff(ovo.getMon_dayoff());
 		if (ovo2.getMon_dayoff()==null) {
@@ -78,6 +106,7 @@ public class OperatingTime {
 		ovo2.setSat_dayoff(ovo.getSat_dayoff());
 		if (ovo2.getSat_dayoff()==null) {
 			ovo2.setSat_dayoff("F");
+			
 		}
 		ovo2.setSun_dayoff(ovo.getSun_dayoff());
 		if (ovo2.getSun_dayoff()==null) {
