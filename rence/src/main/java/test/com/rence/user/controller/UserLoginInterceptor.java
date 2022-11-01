e6d1a117bd226516d8b77b5cb4e5e812a24f7c4f
KANGKYEONGSEOK <rudtjr1303@naver.com>
2022-11-01 16:33:38 +0900

    [refactor] UserLoginInterceptor에 servlet추가 및 불필요 컨트롤러 파일 삭제

--- rence/src/main/java/test/com/rence/user/controller/UserLoginInterceptor.java
+++ rence/src/main/java/test/com/rence/user/controller/UserLoginInterceptor.java
@@ -26,11 +26,26 @@ public class UserLoginInterceptor extends HandlerInterceptorAdapter {
 
 		//221027기준 유저의 기본적인 페이지 servlet 기입한 상태임. 
 		//추후 예약페이지나 기타페이지를 controller에서 구현후 추가 기입이 필요함 (추후 완료시 주석 제거하기!!!)
+		//221101기준 마이페이지외 상세 페이지 
 		if (sPath.equals("/go_my_page") ||sPath.equals("/selectOne")
-			|| sPath.equals("/user_delete.do") || sPath.equals("/user_deleteOK.do")){
-			//로그인이 되지 않으면 로그인 페이지로 이동
+			|| sPath.equals("/secedeOK")
+			|| sPath.equals("/go_now_reserve") || sPath.equals("/go_before_reserve")
+			|| sPath.equals("/go_mileage") || sPath.equals("/mileage_search_list")
+			|| sPath.equals("/go_question_list") || sPath.equals("user_pw_updateOK")
+			|| sPath.equals("/check_now_pw") || sPath.equals("/user_img_updateOK")
+			|| sPath.equals("/reserve_list") || sPath.equals("/review_list")
+			|| sPath.equals("/delete_review") || sPath.equals("/question_list")
+			|| sPath.equals("/reserve_detail_before") || sPath.equals("/reserve_detail_now")
+			|| sPath.equals("/user_logoutOK") || sPath.equals("")
+//			|| sPath.equals("") || sPath.equals("")
+//			|| sPath.equals("") || sPath.equals("")
+//			|| sPath.equals("") || sPath.equals("")
+//			|| sPath.equals("") || sPath.equals("")
+			)
+		{
+			//로그인이 되지 않으면 홈페이지로 이동
 			if (user_id == null) {
-				response.sendRedirect("login.do");
+				response.sendRedirect(".home");
 				return false;
 			}
 		}
