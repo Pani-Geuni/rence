77af8609264087f4332b09201bb30d5c8020099e
PanGeun <lifebook0809@gmail.com>
2022-11-02 13:14:52 +0900

    [refactor] space detail tiles 추가

--- rence/src/main/java/test/com/rence/office/controller/OfficeController.java
+++ rence/src/main/java/test/com/rence/office/controller/OfficeController.java
@@ -109,6 +109,87 @@ public class OfficeController {
 		model.addAttribute("otvo", otvo);
 		
 		
-		return ".space";
+		return ".space/space_detail_introduce";
+	}
+	
+	
+	// **********************
+	// space introduce OFFICE
+	// **********************
+	@RequestMapping(value = "/space_introduce_office", method = RequestMethod.GET)
+	public String space_intruduce_office(BackOfficeVO bvo, String introduce_menu, Model model) {
+		
+		OfficeInfoMap info_map = new OfficeInfoMap();
+		
+		logger.info("space_introduce()...");
+		
+		String backoffice_no = bvo.getBackoffice_no();
+		
+		// ******************
+		// backoffice 기본 정보
+		// ******************
+		OfficeInfoVO ovo = service.select_one_office_info(backoffice_no);
+		List<String> type_list = new ArrayList<String>();
+		List<String> tag_list = new ArrayList<String>();
+		List<String> img_list = new ArrayList<String>();
+		List<String> option_list = new ArrayList<String>();
+		List<String> around_option_list = new ArrayList<String>();
+		
+		if (ovo.getBackoffice_type() != null) {
+			type_list = info_map.splitType(ovo.getBackoffice_type());			
+		} else {
+			type_list.add("타입 없음");
+		}
+		
+		if (ovo.getBackoffice_tag() != null) {
+			tag_list = info_map.splitTag(ovo.getBackoffice_tag());			
+		} else {
+			tag_list.add("옵션 없음");
+		}
+		
+		img_list = info_map.splitImage(ovo.getBackoffice_image());
+		
+		if (ovo.getBackoffice_option() != null) {
+			option_list = info_map.splitOption(ovo.getBackoffice_option());			
+		} else {
+			option_list.add("옵션 없음");
+		}
+		
+		if (ovo.getBackoffice_around() != null) {
+			around_option_list = info_map.splitAroundOption(ovo.getBackoffice_around());			
+		} else {
+			around_option_list.add("주변 시설 없음");
+		}
+		
+		ovo.setShort_roadname_address(info_map.makeShortAddress(ovo.getRoadname_address()));
+		
+		// ******************
+		// backoffice 운영 시간
+		// ******************
+		OfficeOperatingTimeVO_date dotvo = service.select_one_operating_time(backoffice_no);
+		
+		CustomDateFormatter df = new CustomDateFormatter();
+		OfficeOperatingTimeVO otvo = df.showStringOfficeOperating(dotvo);
+		
+		// ************************
+		// backoffice 운영 공간(Room)
+		// ************************
+		List<OfficeRoomVO> rvo = service.select_all_room(backoffice_no);
+		
+		logger.info("rvo :: {}", rvo);
+		
+		// backoffice 기본 정보
+		model.addAttribute("ovo", ovo);
+		model.addAttribute("type_list", type_list);
+		model.addAttribute("tag_list", tag_list);
+		model.addAttribute("img_list", img_list);
+		model.addAttribute("option_list", option_list);
+		model.addAttribute("around_option_list", around_option_list);
+
+		// backoffice 운영 시간
+		model.addAttribute("otvo", otvo);
+		
+		
+		return ".space/space_detail_introduce_office";
 	}
 }
