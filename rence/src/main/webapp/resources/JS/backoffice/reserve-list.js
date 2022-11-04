54aaa16b19dad9b9ef3d00b6bb0494ccf8ed8f88
wtw <qjspdl@hs.ac.kr>
2022-11-04 11:11:56 +0900

    [feat] 예약관리 메뉴 클릭 이벤트 구현

--- rence/src/main/webapp/resources/JS/backoffice/reserve-list.js
+++ rence/src/main/webapp/resources/JS/backoffice/reserve-list.js
@@ -2,25 +2,44 @@
  * @author 김예은
  */
 $(function() {
-  // // CUSTOM TYPE SELECT
-  // $('#type').click(function(){
-  //   $('#custom-type-select').toggleClass('blind');
-  // });
-
-  // $('.type-select-list').click(function(e){
-  //   $('#type>span').text($(this).text());
-  //   $('#custom-type-select').addClass('blind');
-  // });
-
   $("#input_searchBar").on('keydown', function(e){
     if(e.keyCode == 13){
-        if($("#input_searchBar").val().trim().length > 0){
-        location.href="/rence/backoffice_search_reserve?backoffice_no=" + $.cookie("backoffice_no") + "&searchword=" + $("#input_searchBar").val().trim();
-        }else{
-          $(".popup-background:eq(1)").removeClass("blind");
-          $("#common-alert-popup").removeClass("blind");
-          $(".common-alert-txt").text("검색어를 입력하세요.");
-        }
+      var reserve_state = "";
+      switch($(".reserve-item.active").text()){
+        case "전체" :
+          reserve_state = "all";
+          break;
+        case "예약중" :
+          reserve_state = "in_use";
+          break;
+        case "취소" :
+          reserve_state = "cancle";
+          break;
+        case "종료" :
+          reserve_state = "end";
+          break;
       }
+      location.href="/rence/backoffice_search_reserve?backoffice_no=" + $.cookie("backoffice_no") + "&reserve_state="+ reserve_state + "&searchword=" + $("#input_searchBar").val().trim();
+    }
+  });
+
+  // 미니 메뉴 부분 클릭 이벤트
+  $(".reserve-item").click(function(){
+    var reserve_state = "";
+      switch($(this).text()){
+        case "전체" :
+          reserve_state = "all";
+          break;
+        case "예약중" :
+          reserve_state = "in_use";
+          break;
+        case "취소" :
+          reserve_state = "cancle";
+          break;
+        case "종료" :
+          reserve_state = "end";
+          break;
+      }
+    location.href="/rence/backoffice_reserve?backoffice_no=" + $.cookie("backoffice_no") + "&reserve_state="+ reserve_state;
   });
 });
