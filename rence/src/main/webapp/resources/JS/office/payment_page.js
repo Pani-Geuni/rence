bacda92e1a96fe31af9f35408623294ddf6f0403
PanGeun <lifebook0809@gmail.com>
2022-11-05 14:03:40 +0900

    [refactor] 12시간 버그 수정

--- rence/src/main/webapp/resources/JS/office/payment_page.js
+++ rence/src/main/webapp/resources/JS/office/payment_page.js
@@ -3,8 +3,6 @@
  */
  
 $(function() {
- 	var IMP = window.IMP;
- 	IMP.init("TC0ONETIME");
 	
 	// ****************
 	// 가격에 ,(콤마) 붙이기
@@ -132,38 +130,5 @@ $(function() {
 		);
 			
 		$("#payment_all").attr("payment_all", actual_payment);
-	});
-	
-	
-	$(".pay-btn").click(function() {
-		IMP.request_pay({ // param
-          	pg : 'kakaopay',
-		    pay_method : 'card', //생략 가능
-		    merchant_uid: "order_no_0001", // 상점에서 관리하는 주문 번호
-		    name : '주문명:결제테스트',
-		    amount : 14000,
-		    buyer_email : 'iamport@siot.do',
-		    buyer_name : '구매자이름',
-		    buyer_tel : '010-1234-5678',
-		    buyer_addr : '서울특별시 강남구 삼성동',
-		    buyer_postcode : '123-456'
-      }, function (rsp) { // callback
-      if (rsp.success) { // 결제 성공 시: 결제 승인 또는 가상계좌 발급에 성공한 경우
-        // jQuery로 HTTP 요청
-        jQuery.ajax({
-            url: "{서버의 결제 정보를 받는 endpoint}", // 예: https://www.myservice.com/payments/complete
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            data: {
-                imp_uid: rsp.imp_uid,
-                merchant_uid: rsp.merchant_uid
-            }
-        }).done(function (data) {
-          // 가맹점 서버 결제 API 성공시 로직
-        })
-      } else {
-        alert("결제에 실패하였습니다. 에러 내용: " +  rsp.error_msg);
-      }
-    });
 	});
 })
