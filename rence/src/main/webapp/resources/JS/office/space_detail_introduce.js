eefee64366c8e8cda566f50328d253e72f61ad27
wtw <qjspdl@hs.ac.kr>
2022-11-06 21:49:00 +0900

    [feat] 문의 구현중

--- rence/src/main/webapp/resources/JS/office/space_detail_introduce.js
+++ rence/src/main/webapp/resources/JS/office/space_detail_introduce.js
@@ -1,8 +1,17 @@
 /**
  * @author 김예은, 전판근
  */
-$(function(){
+ $(function(){
   let test = 1;
+
+  $("#common-alert-btn").click(function(){
+    $(".popup-background:eq(1)").addClass("blind");
+    $("#common-alert-popup").addClass("blind");
+
+    if($(".common-alert-txt").text() == "성공적으로 문의가 등록되었습니다." || $(".common-alert-txt").text() == "공간이 추가되었습니다."){
+      location.reload();
+    }
+  });
 
   /***** ************** *****/ 
   /***** 슬라이드 이미지 *****/ 
@@ -76,57 +85,64 @@ $(function(){
   /*** 예약 가능 여부 버튼 클릭 ***/
   $("#check_available").click(function(){
     // 예약 타입 선택 O
-    if($(".type-border-txt").prop("check")){
+    if($(".type-border-txt").prop("check") == true){
       if($(".time-input:eq(0)").val() != '' && $(".time-input:eq(1)").val() != ''){
-        // 예약 가능 확인 로직
-
-        let query = location.search;
-        let param = new URLSearchParams(query);
-        let backoffice_no = param.get('backoffice_no');
-        let room_no = $("#type-choice-value").attr("room_no");
-        let reserve_stime = $(".time-input:eq(0)").val();
-        let reserve_etime = $(".time-input:eq(1)").val();
-                
-        $.ajax({
-        	url : "/rence/reserve_checkOK",
-        	type : "POST",
-        	dataType : "json",
-        	data : {
-        		reserve_stime : reserve_stime,
-        		reserve_etime : reserve_etime,
-        		room_no : room_no,
-        		backoffice_no : backoffice_no,
-        		user_no : $.cookie("user_no")
-        	},
-        	
-        	success : function(res) {
-        		
-				if (res.result == 1) {
-					console.log("success");
-					location.href = "/rence/payment_page?reserve_no=" + res.reserve_no;
-				} else if (res.result == 0) {
-					console.log("fail");
-					$(".fixed-popup").removeClass("blind");
-        			$(".using-time-fail-txt").html("해당 시간은 예약 할 수 없습니다.");
-				}
-        	},
-        	error : function(error) {
-        		console.log(error)
-        	}
-        	
-        });
-        
+        if($(".time-input:eq(0)").val() > $(".time-input:eq(1)").val()){
+          $(".fixed-popup").removeClass("blind");
+          $(".using-time-fail-txt:eq(0)").html("체크인 시간이 체크아웃 시간보다<br><br>빠를 수는 없습니다.");
+        }
+        else{
+          if($(".time-input:eq(0)").val() == $(".time-input:eq(1)").val()){
+            $(".fixed-popup").removeClass("blind");
+            $(".using-time-fail-txt:eq(0)").html("체크인 시간과 체크아웃 시간이<br><br>같을 수는 없습니다.");
+          }
+          else{
+            // 예약 가능 확인 로직
+            let query = location.search;
+            let param = new URLSearchParams(query);
+            let backoffice_no = param.get('backoffice_no');
+            let room_no = $("#type-choice-value").attr("room_no");
+            let reserve_stime = $(".time-input:eq(0)").val();
+            let reserve_etime = $(".time-input:eq(1)").val();
+                    
+            $.ajax({
+              url : "/rence/reserve_checkOK",
+              type : "POST",
+              dataType : "json",
+              data : {
+                reserve_stime : reserve_stime,
+                reserve_etime : reserve_etime,
+                room_no : room_no,
+                backoffice_no : backoffice_no,
+                user_no : $.cookie("user_no")
+              },
+              
+              success : function(res) {
+                if (res.result == 1) {
+                  console.log("success");
+                  location.href = "/rence/payment_page?reserve_no=" + res.reserve_no;
+                } else if (res.result == 0) {
+                  $(".fixed-popup").removeClass("blind");
+                  $(".using-time-fail-txt:eq(0)").html("해당 시간은 예약 할 수 없습니다.");
+                }
+              },
+              error : function(error) {
+                console.log(error)
+              }
+            });
+          }
+        }
       }
       // 예약 타입 선택 O, 체크인 or 체크아웃 시간 X
       else{
         $(".fixed-popup").removeClass("blind");
-        $(".using-time-fail-txt").html("체크인 시간과 체크아웃 시간을<br><br>모두 선택하여 주십시오.");
+        $(".using-time-fail-txt:eq(0)").html("체크인 시간과 체크아웃 시간을<br><br>모두 선택하여 주십시오.");
       }
     }
     // 예약 타입 선택 X
     else{
       $(".fixed-popup").removeClass("blind");
-      $(".using-time-fail-txt").html("예약 타입을 선택하여 주십시오.");
+      $(".using-time-fail-txt:eq(0)").html("예약 타입을 선택하여 주십시오.");
     }
   });
 
@@ -216,6 +232,7 @@ $(function(){
   /***** ************** *****/ 
   /***** QUESTION POPUP *****/ 
   /***** ************** *****/
+
   /*** 팝업 닫기 버튼 클릭  ***/
   $("#question-close-btn").click(function(){
     // TEXTAREA 초기화
@@ -223,14 +240,24 @@ $(function(){
     // 글자수 초기화
     $(".qna-length").text("0");
 
-    $(".question-popup-select").addClass("blind");
-    $(".question-popup-select-value").text("타입을 선택해 주세요");
-    $(".question-popup-select-val-wrap").removeClass("open-select");
+    $("#question-write").removeClass("null-input-border");
+    $(".question-popup-select-val-wrap:eq(0)").removeClass("null-input-border");
+    $(".question-popup-select-val-wrap:eq(0)").removeClass("open-select");
+    
+    $("#question-select-choice").text("타입을 선택해 주세요");
+    $("#question-select-choice").attr("choice_idx", "");
+    $("#question-select-choice").attr("choice", "");
+    $(".question-popup-select:eq(0)").addClass("blind");
     
     // 팝업 닫기
     $("#question-popup").addClass("blind");
   });
 
+  /*** 문의 작성 경고 테두리 제거  ***/
+  $("#question-write").click(function(){
+    $("#question-write").removeClass("null-input-border");
+  });
+  
   /*** 문의 작성 글자수 제한  ***/
   $("#question-write").on("keydown keyup", function(){
     if($(this).val().length > 500){
@@ -239,21 +266,80 @@ $(function(){
     $(".qna-length").text($(this).val().length);
   });
 
-  $("#question-popup").click(".question-popup-select-val-wrap", function(){
-    if($(".question-popup-select").hasClass("blind")){
-      $(".question-popup-select-val-wrap").addClass("open-select");
-      $(".question-popup-select").removeClass("blind");
+  $(".question-popup-select-val-wrap:eq(0)").click(function(){
+    if($(".question-popup-select:eq(0)").hasClass("blind")){
+      $(".question-popup-select-val-wrap:eq(0)").addClass("open-select");
+      $(".question-popup-select:eq(0)").removeClass("blind");
+      $(".question-popup-select-val-wrap:eq(0)").removeClass("null-input-border");
     }
     else{
-      $(".question-popup-select-val-wrap").removeClass("open-select");
-      $(".question-popup-select").addClass("blind");
+      $(".question-popup-select-val-wrap:eq(0)").removeClass("open-select");
+      $(".question-popup-select:eq(0)").addClass("blind");
     }
   });
 
   /** 팝업 셀렉트 리스트 클릭 이벤트 */
-  $(".question-popup-select-li").click(function(){
+  $("#question-popup").on("click", ".question-popup-select-li", function(){
     $("#question-select-choice").text($(this).text());
-    $(".question-popup-select-val-wrap").removeClass("open-select");
-    $(".question-popup-select").addClass("blind");
+    $("#question-select-choice").attr("choice", "true");
+    $("#question-select-choice").attr("choice_idx", $(this).attr("idx"));
+
+    $(".question-popup-select-val-wrap:eq(0)").removeClass("open-select");
+    $(".question-popup-select:eq(0)").addClass("blind");
+  });
+
+  $("#question-createBtn").click(function(){
+    if($("#question-select-choice").attr("choice") == "true" && $("#question-write").val().trim().length > 0){
+        $.ajax({
+          url : "/rence/insert_question",
+          type : "GET",
+          dataType : 'json',
+          data : {
+              user_no : $.cookie("user_no"),
+              backoffice_no : location.href.split("backoffice_no=")[1].split("&")[0],
+              room_no : $("#question-select-choice").attr("choice_idx"),
+              comment_content : $("#question-write").val().trim()
+          },
+          success : function(res) {
+              if(res.result == 1){
+                $(".qna-length").text("0");
+                $("#question-write").val("");
+
+                $(".question-popup-select-val-wrap:eq(0)").removeClass("null-input-border");
+                $("#question-write").removeClass("null-input-border");
+                
+                $("#question-select-choice").text("타입을 선택해 주세요");
+                $("#question-select-choice").attr("choice_idx", "");
+                $("#question-select-choice").attr("choice", "");
+                
+                $(".question-popup-select-val-wrap:eq(0)").removeClass("open-select");
+                $(".question-popup-select:eq(0)").addClass("blind");
+                $("#question-popup").addClass("blind");
+
+                $(".popup-background:eq(1)").removeClass("blind");
+                $("#common-alert-popup").removeClass("blind");
+                $(".common-alert-txt").text("성공적으로 문의가 등록되었습니다.");
+              }else{
+                $(".popup-background:eq(1)").removeClass("blind");
+                $("#common-alert-popup").removeClass("blind");
+                $(".common-alert-txt").text("비밀번호가 일치하지않습니다.");
+              }
+          },
+          error : function(error) {
+              console.log(error);
+              $(".popup-background:eq(1)").removeClass("blind");
+              $("#common-alert-popup").removeClass("blind");
+              $(".common-alert-txt").text("오류 발생으로 인해 처리에 실패하였습니다.");
+          }
+      });
+      }
+      else{
+        if($("#question-write").val().trim().length == 0){
+          $("#question-write").addClass("null-input-border");
+        }
+        if($("#question-select-choice").attr("choice") != "true"){
+          $(".question-popup-select-val-wrap:eq(0)").addClass("null-input-border");
+        }
+    }
   });
 });
